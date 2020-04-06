0546bd032d15091487119be896d8da1a38f3b4d2
Darlan de Castro <darlandc@gmail.com>
2020-04-05 23:28:27 -0300

    playin with text=fields

--- src/app/item/items.component.ts
+++ src/app/item/items.component.ts
@@ -3,6 +3,9 @@ import { Component, OnInit } from "@angular/core";
 import { Item } from "./item";
 import { ItemService } from "./item.service";
 
+import { TextField } from "tns-core-modules/ui/text-field";
+import { setTimeout } from "tns-core-modules/timer";
+
 @Component({
     selector: "ns-items",
     templateUrl: "./items.component.html"
@@ -10,9 +13,57 @@ import { ItemService } from "./item.service";
 export class ItemsComponent implements OnInit {
     items: Array<Item>;
 
-    constructor(private itemService: ItemService) { }
+    userData = {
+        name: 'Darlan de Castro',
+        level: 999
+    }
+
+    constructor(private itemService: ItemService) {
+        console.log(this.userData)
+    }
 
     ngOnInit(): void {
         this.items = this.itemService.getItems();
+
+        for(let i = 0; i<=10; i++){
+            console.log('.')
+        }
+
+        console.log(this.userData)
+
+
+
+
     }
+
+    name = "";
+
+    onReturnPress(args) {
+        // returnPress event will be triggered when user submits a value
+        let textField = <TextField>args.object;
+        // Gets or sets the input text.
+        console.log(textField.text);
+        // Gets or sets the soft keyboard type. Options: "datetime" | "phone" | "number" | "url" | "email"
+        console.log(textField.keyboardType);
+        // Gets or sets the soft keyboard return key flavor. Options: "done" | "next" | "go" | "search" | "send"
+        console.log(textField.returnKeyType);
+
+        setTimeout(() => {
+            textField.dismissSoftInput(); // Hides the soft input method, ususally a soft keyboard.
+        }, 100);
+    }
+
+    onFocus(args) {
+        // focus event will be triggered when the users enters the TextField
+        let textField = <TextField>args.object;
+    }
+
+    onBlur(args) {
+        // blur event will be triggered when the user leaves the TextField
+        let textField = <TextField>args.object;
+        console.log(textField.text)
+        this.userData.name = textField.text
+        console.log(this.userData)
+    }
+
 }
